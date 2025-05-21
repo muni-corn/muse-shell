f6eaae3a9cd278a039526b2eef600ac98bb169ce
municorn <municorn@musicaloft.com>
2025-05-20 23:48:25 -0600

    refactor: split App init object into functions

--- app.ts
+++ app.ts
@@ -6,30 +6,36 @@ import {
   NotificationPopups,
 } from "./widget/notifications/notifications.tsx";
 
+function main() {
+  const monitors = App.get_monitors();
+
+  // show bar on all monitors
+  monitors.map(Bar);
+
+  // show notifications on last monitor
+  NotificationPopups(monitors[monitors.length - 1]);
+}
+
+// this runs in the main instance
+function requestHandler(request: string, res: (response: unknown) => void) {
+  if (request === "noti-act") {
+    NotificationMap.get_default().activateTopNotification();
+    res("done");
+  }
+}
+
+function client(message: (msg: string) => string, ...args: string[]): void {
+  if (args[0] === "noti" && args[1] === "act") {
+    message("noti-act");
+  }
+}
+
 App.start({
   css: style,
   instanceName: "muse-shell",
-  main() {
-    const monitors = App.get_monitors();
 
-    // show bar on all monitors
-    monitors.map(Bar);
-
-    // show notifications on last monitor
-    NotificationPopups(monitors[monitors.length - 1]);
-  },
-
-  // this runs in the main instance
-  requestHandler(request: string, res: (response: unknown) => void) {
-    if (request === "noti-act") {
-      NotificationMap.get_default().activateTopNotification();
-      res("done");
-    }
-  },
-
-  client(message: (msg: string) => string, ...args: string[]): void {
-    if (args[0] === "noti" && args[1] === "act") {
-      message("noti-act");
-    }
-  },
+  // functions
+  main,
+  requestHandler,
+  client,
 });
